58ffb0e93594a0229c35021ace26b5b5821a8fc5
ArjunS <writetoarjun1@gmail.com>
2025-10-04 12:05:12 -0500

    Added python File for data anylsis for Cumlative DATA CSV

---
+++ Code/CumulativeDataAnlysis/CumulativeMain.py
@@ -0,0 +1,8 @@
+#imports
+import pandas as pd
+
+#CODE :)
+
+#Importing Data
+CumalitveDataCSV = pd.read_csv('C:/Users/write/OneDrive/Documentos/GitHub/SpaceInvaders-NASASpaceApps/Dat/acumulative_2025.10.04_09.06.58.csv')
+print(CumalitveDataCSV)
